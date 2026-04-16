c994983f2a0f73704804fab63ad18715ab97e7f5
Mubarak Lawal <Mubaraklawal52@gmail.com>
2026-04-16 15:46:47 +0000

    implemented validator

--- phone_ng/lib/src/validator.dart
+++ phone_ng/lib/src/validator.dart
@@ -1,7 +1,25 @@
+import 'package:phone_ng/src/models.dart';
+import 'package:phone_ng/src/normalizer.dart';
 import 'package:phone_ng/src/parser.dart';
+import 'package:phone_ng/src/prefixes.dart';
 
-bool isValid(String input){
-  final result = parse(input);
+bool isValid(String input) {
+  final ParseResult result = parse(input);
+  return result is ParseSuccess;
+}
 
-  return r
+bool isPossible(String input) {
+  final normalized = normalize(input);
+
+  if (normalized is NormalizeFailure) {
+    return false;
+  }
+
+  final String digits = (normalized as NormalizeSuccess).digits;
+
+  if (digits.length != mobileLength) {
+    return false;
+  }
+
+  return true;
 }
